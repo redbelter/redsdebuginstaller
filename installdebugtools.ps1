e824b25f684658b9b0314a2da9ad31d7918aa8b0
redbelter <jupiterrun@proton.me>
2024-07-13 00:26:44 -0700

    Update installdebugtools.ps1
    fgfd

--- installdebugtools.ps1
+++ installdebugtools.ps1
@@ -1,4 +1,6 @@
-﻿new-item "C:\tools\" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+﻿
+
+new-item "C:\tools\" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://download.sysinternals.com/files/PSTools.zip","C:\tools\pstools.zip")
 Expand-Archive -LiteralPath "C:\tools\pstools.zip" -DestinationPath "C:\tools\" -Force
@@ -9,3 +11,36 @@ Expand-Archive -LiteralPath "C:\tools\processmonitor.zip" -DestinationPath "C:\t
 
 $WebClient.DownloadFile("https://download.sysinternals.com/files/ProcessMonitor.zip","C:\tools\processexplorer.zip")
 Expand-Archive -LiteralPath "C:\tools\processexplorer.zip" -DestinationPath "C:\tools\"  -Force
+
+$WebClient.DownloadFile("https://go.microsoft.com/fwlink/?linkid=2272610","C:\tools\winsdk.exe")
+$WebClient.DownloadFile("https://go.microsoft.com/fwlink/?linkid=2272234","C:\tools\winwdk.exe")
+$sdkinstall = "C:\tools\winsdk.exe"
+$wdkinstall = "C:\tools\winwdk.exe"
+
+write-host "uninstall sdk, wdk just incase"
+Start-Process -FilePath $sdkinstall -ArgumentList " /uninstall /install C:\tools\sdk /q" -NoNewWindow -Wait
+Start-Process -FilePath $wdkinstall -ArgumentList " /uninstall /install C:\tools\sdk /q" -NoNewWindow -Wait
+Get-ChildItem "C:\tools\sdk" -Recurse | Remove-Item -Recurse
+
+write-host "start with sdk"
+
+#install windows sdk
+#https://go.microsoft.com/fwlink/?linkid=2272610
+$sdkargs = " /features + /installpath C:\tools\sdk /q"
+Start-Process -FilePath $sdkinstall -ArgumentList $sdkargs -NoNewWindow -Wait
+write-host "done with sdk"
+
+write-host "start with wdk"
+#download the wdk
+#https://go.microsoft.com/fwlink/?linkid=2272234
+
+
+$wdkargs = " /features + /installpath C:\tools\sdk /q"
+Start-Process -FilePath $wdkinstall -ArgumentList $wdkargs -NoNewWindow -Wait
+write-host "done with wdk"
+
+#repair the sdk to get tracefmt
+write-host "repair sdk to get tracefmt"
+$sdkargs = " /features + /installpath C:\tools\sdk /q /repair"
+Start-Process -FilePath $sdkinstall -ArgumentList $sdkargs -NoNewWindow -Wait
+write-host "done with sdk"
